7f0ee35bc6e8a3fc76f7b3c8bc7a4acafb371198
Dddestroyer10 <meenusharda@yahoo.com>
2021-09-03 00:55:24 +0530

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import TransactionScreen from './screens/CycleTransactionScreen'
+import Searchscreen from './screens/SearchScreen'
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return(
+      <SafeAreaProvider>
+      <AppContainer />
+      </SafeAreaProvider>
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  TransactionScreen: TransactionScreen,
+  Searchscreen: Searchscreen
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "TransactionScreen"){
+        return(
+          <Image
+          source={require("./assets/book.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "Searchscreen"){
+        return(
+          <Image
+          source={require("./assets/searchingbook.png")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+}
+  )
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
